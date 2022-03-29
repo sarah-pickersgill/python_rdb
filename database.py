7c9e423710e9696234df387af99df551934fd103
Sarah <sarahjp@uw.edu>
2022-03-29 13:28:22 -0700

    add table

--- database.py
+++ database.py
@@ -6,3 +6,20 @@ conn = sqlite3.connect('test.db')
 #or use an existing database in memory
 #conn = sqlite3.connect(':memory:')
 
+#create a cursor
+c = conn.cursor()
+
+#create table 
+c.execute("""CREATE TABLE customers (
+		first_name text,
+		last_name text,
+		email text,
+		value real
+	)""")
+
+#commit
+conn.commit()
+
+#close
+conn.close()
+
